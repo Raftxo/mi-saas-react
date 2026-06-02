2719b6a17becb0dda755ccc07c875f27ad2dcdc1
Raftxo <fatwhiteduke@gmail.com>
2026-06-02 19:29:32 +0200

    Create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+// componente principal: App
+// 1. Importamos la hoja de estilos App.css
 import './App.css';
-
+// 2. Importamos las dos grandes mitades de nuestra pantalla: Sidebar y ChatArea
+import Sidebar from './Sidebar';
+import ChatArea from './ChatArea';
 function App() {
   return (
-    <>
-      <div>
-        <h1>¡Hola React! Nuestro entorno está Listo. 🚀</h1>
-      </div>
-    </>
+    // Contenedor principal que usa flexbox (definido en App.css)
+    // para poner las cosas lado a lado
+    <div className="app-container">
+      {/* inyectamos la mitad izquierda */}  
+        <Sidebar />
+      {/* inyectamos la mitad derecha */}
+        <ChatArea />  
+    </div>
+    
   );
 }
 
